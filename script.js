9f0700139b0276dbdc91b92f877b8d2d976c6255
Irene <irene.a.c98@gmail.com>
2024-12-03 13:09:52 +0100

    actualizado

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
+//Variables para los puntos
 let puntosJugador = 0
 let puntosOrdenador = 0
 
-function empezarJuego(eleccion_jugador){
+//Función para empezar el juego
+function empezarJuego(eleccionJugador){
     const elecciones = ['piedra', 'papel', 'tijera']
-    const eleccion_ordenador = elecciones[Math.floor(Math.random() * 3)]
-    resultado = ''
+    const eleccionOrdenador = elecciones[Math.floor(Math.random() * 3)]
+    let resultado = ''
 
-    if(eleccion_jugador === eleccion_ordenador) {
-        `Empate. Ambos habéis seleccionado ${eleccion_jugador}`
+    if(eleccionJugador === eleccionOrdenador) {
+        resultado = `Empate. Ambos habéis seleccionado ${eleccionJugador}`
     } else if(
-        (eleccion_jugador === 'piedra' && eleccion_ordenador === 'tijera') ||
-        (eleccion_jugador === 'papel' && eleccion_ordenador === 'piedra') ||
-        (eleccion_jugador === 'tijera' && eleccion_ordenador === 'papel') 
-    ){
-        resultado = `Jugador gana un punto. ${eleccion_jugador} vence a ${eleccion_ordenador}`
-        puntosJugador++
-    } else {
-        resultado = `Máquina gana un punto. ${eleccion_ordenador} vence a ${eleccion_jugador}`
-        puntosOrdenador++
-    }
-
+        (eleccionJugador === 'piedra' && eleccionOrdenador === 'tijera') ||
+        (eleccionJugador === 'papel' && eleccionOrdenador === 'piedra') ||
+        (eleccionJugador === 'tijera' && eleccionOrdenador === 'papel')) {
+           resultado = `¡Ganas un punto! ${eleccionJugador} vence a ${eleccionOrdenador}`
+            puntosJugador++
+        } else {
+            resultado = `Máquina gana un punto, ${eleccionOrdenador} vence a ${eleccionJugador}`
+            puntosOrdenador++
+        }
+    
     document.getElementById('resultado').textContent = resultado
     document.getElementById('puntos').textContent = `Jugador: ${puntosJugador} | Máquina: ${puntosOrdenador}`
-
+    
     if(puntosJugador === 3 || puntosOrdenador === 3) {
-        acabarJuego()
+        finJuego()
     }
 }
 
-function acabarJuego(){
-    const resultadoFinal = puntosJugador === 3 ? `¡Felicidades, has ganado la partida! :)` : `Upss, lo siento máquina ha ganado la partida :( `
-    document.getElementById('resultado').textContent = resultadoFinal
+//Función para finalizar el juego
+function finJuego(){
+    const finalResultado = puntosJugador === 3 ? '¡Felicidades, has ganado! :)' : 'Upss, lo siento, ha ganado la máquina :('
+    document.getElementById('resultado').textContent = finalResultado
 
     document.getElementById('botonPiedra').disabled = true
     document.getElementById('botonPapel').disabled = true
     document.getElementById('botonTijera').disabled = true
 }
+
 
